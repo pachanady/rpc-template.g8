0a8f270c3a87de51d472079a5f1a448bde7d06da
Ramesh Pachanady <Ramesh.Pachanady@bigcommerce.com>
2020-04-21 18:55:38 -0700

    Removed $

--- src/main/g8/$common$/src/main/scala/com/bigcommerce/common/errors/Error.scala
+++ src/main/g8/$common$/src/main/scala/com/bigcommerce/common/errors/Error.scala
@@ -9,7 +9,7 @@ trait NotFoundError extends Error
 
 trait FieldsError extends Error {
   val fieldErrors: Seq[FieldError]
-  def message = s"The following fields are invalid: ${fieldErrors.map(_.field).mkString(", ")}."
+  def message = s"The following fields are invalid: //TO DO ${fieldErrors.map(_.field).mkString(", ")}."
 }
 
 case class FieldError(field: String, message: String, code: String = "")
