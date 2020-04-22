884e09e722c59db975c546db8890814598b272e5
Ramesh Pachanady <Ramesh.Pachanady@bigcommerce.com>
2020-04-21 19:06:24 -0700

    Padded $ with \$

--- src/main/g8/$module$/src/main/scala/com.bigcommerce.$module$/$module$ServiceImpl.scala
+++ src/main/g8/$module$/src/main/scala/com.bigcommerce.$module$/$module$ServiceImpl.scala
@@ -30,8 +30,7 @@ import com.google.inject.Guice
 import io.grpc.{ServerInterceptors, ServerServiceDefinition}
 import io.prometheus.client.CollectorRegistry
 
-class $$module;format="Camel"$
-class $$module;format="Camel"$
+class $module;format="Camel"$
   extends RpcServer {
 
 
